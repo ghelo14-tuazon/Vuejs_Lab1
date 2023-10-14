491254e6e1e5fa420deca0e338faacc841127c87
ghelo14-tuazon <gtma675@gmail.com>
2023-10-14 16:30:31 +0800

    Update HelloWorld.vue

--- vuejs/src/components/HelloWorld.vue
+++ vuejs/src/components/HelloWorld.vue
@@ -4,18 +4,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Biolife - Organic Food</title>
-    <link href="https://fonts.googleapis.com/css?family=Cairo:400,600,700&amp;display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Poppins:600&amp;display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400i,700i" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Ubuntu&amp;display=swap" rel="stylesheet">
-    <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.png"/>
-    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/animate.min.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/nice-select.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/slick.min.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/main-color.css">
+    
 
 </head>
 <body class="biolife-body">
@@ -3288,3 +3277,17 @@ export default {
   }),
 }
 </script>
+<style scoped>
+@import 'https://fonts.googleapis.com/css?family=Cairo:400,600,700&display=swap';
+@import 'https://fonts.googleapis.com/css?family=Poppins:600&display=swap';
+@import 'https://fonts.googleapis.com/css?family=Playfair+Display:400i,700i';
+@import 'https://fonts.googleapis.com/css?family=Ubuntu&display=swap';
+
+@import '@/assets/css/bootstrap.min.css';
+@import '@/assets/css/animate.min.css';
+@import '@/assets/css/font-awesome.min.css';
+@import '@/assets/css/nice-select.css';
+@import '@/assets/css/slick.min.css';
+@import '@/assets/css/style.css';
+@import '@/assets/css/main-color.css';
+</style>
