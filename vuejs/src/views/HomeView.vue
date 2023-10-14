6289a4a663537c31d3b921fb141c72d6d5affb25
ghelo14-tuazon <gtma675@gmail.com>
2023-10-14 17:03:43 +0800

    Update HomeView.vue

--- vuejs/src/views/HomeView.vue
+++ vuejs/src/views/HomeView.vue
@@ -1,5 +1,8 @@
 <template>
-  <HelloWorld />
+  <div>
+    <HelloWorld />
+    <Footer />
+  </div>
 </template>
 
 <script>
@@ -7,12 +10,14 @@ import { defineComponent } from 'vue';
 
 // Components
 import HelloWorld from '../components/HelloWorld.vue';
+import Footer from '../components/Footer.vue';
 
 export default defineComponent({
   name: 'HomeView',
 
   components: {
     HelloWorld,
+    Footer,
   },
 });
 </script>
